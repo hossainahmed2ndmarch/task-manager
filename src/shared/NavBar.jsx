90a7f7eeaee77913d97c0b3dd460ff9a2de0b9e7
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2025-02-21 02:04:50 +0600

    Added dark theme feature

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -1,77 +1,80 @@
 import React from "react";
+import useAuth from "../hooks/useAuth";
+import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
+import { useTheme } from "../providers/ThemeProvider";
+import { IoMoonOutline } from "react-icons/io5";
+import { MdOutlineWbSunny } from "react-icons/md";
+import userIcon from "../assets/user.png";
 
 const NavBar = () => {
+  const { user, logOut } = useAuth();
+  const navigate = useNavigate();
+  const { isDark, toggleTheme } = useTheme();
+  // Handle Logout
+  const handleLogOut = () => {
+    toast.info("🚪 You have successfully logged out. See you soon!", {
+      icon: "👋",
+    });
+    logOut();
+    navigate("/");
+  };
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">TaskPilot</a>
       </div>
-      <div className="flex-none">
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                />{" "}
-              </svg>
-              <span className="badge badge-sm indicator-item">8</span>
-            </div>
-          </div>
-          <div
-            tabIndex={0}
-            class="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+      <div className="navbar-end flex items-center space-x-4">
+        <button
+          onClick={toggleTheme}
+          data-tooltip-id="theme-tooltip"
+          data-tooltip-content={
+            isDark ? "Switch to Light Mode" : "Switch to Dark Mode"
+          }
+          className="p-2 rounded-full text-primary hover:bg-primary hover:text-light font-semibold bg-transparent border-none shadow-none text-2xl"
+        >
+          {isDark ? <MdOutlineWbSunny /> : <IoMoonOutline />}
+        </button>
+        {user && user?.email && (
+          <button
+            onClick={handleLogOut}
+            className="btn hidden md:flex border-none bg-transparent text-lg text-white font-bold transition-all"
           >
-            <div className="card-body">
-              <span className="text-lg font-bold">8 Items</span>
-              <span className="text-info">Subtotal: $999</span>
-              <div className="card-actions">
-                <button className="btn btn-primary btn-block">View cart</button>
+            LogOut
+          </button>
+        )}
+        {user && user?.email && (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="User Avatar" src={user?.photoURL || userIcon} />
               </div>
             </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3  p-4 shadow text-black"
+            >
+              {/* User Display Name */}
+              <li
+                className="ml-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full"
+                title={user?.displayName}
+              >
+                {user?.displayName}
+              </li>
+              {/* User Email */}
+              <li
+                className="ml-2 overflow-hidden text-ellipsis whitespace-nowrap max-w-full"
+                title={user?.email}
+              >
+                {user?.email}
+              </li>
+            </ul>
           </div>
-        </div>
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
-        </div>
+        )}
       </div>
     </div>
   );
